97178f9cae607c43f2253bba98ff42774927d00d
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-20 12:38:31 +0500

    Create UserStatus.cs

---
+++ Domain-Service/Enums/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.Enums
+{
+    public enum UserStatus
+    {
+        Active = 1,
+        Inactive = 2,
+        Suspended = 3,
+        Deleted = 4
+    }
+}

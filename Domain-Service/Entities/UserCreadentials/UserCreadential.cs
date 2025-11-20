5174e60038a087e6cfd17c07329882baad79442d
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-20 12:38:21 +0500

    Create UserCreadential.cs

---
+++ Domain-Service/Entities/UserCreadentials/UserCreadential.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.Entities.UserCreadentials
+{
+    public class UserCreadential
+    {
+        public Guid Cread_Id { get; set; }
+        public Guid U_Id { get; set; }
+        public byte[] PasswordHash { get; set; } = [];
+        public byte[] PasswordSalt { get; set; } = [];
+        public string OTP { get; set; } = string.Empty;
+    }
+}

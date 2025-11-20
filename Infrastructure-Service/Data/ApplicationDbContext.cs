e677a433f10326f3de5469046b4a9a479b65ae90
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-20 12:38:34 +0500

    Create ApplicationDbContext.cs

---
+++ Infrastructure-Service/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure_Service.Data
+{
+    public class ApplicationDbContext
+    {
+
+    }
+}

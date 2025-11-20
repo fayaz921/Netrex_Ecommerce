2dd64f982805d7e11446e45ac6da5c79739ed1a4
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-20 12:38:28 +0500

    Create RoleType.cs

---
+++ Domain-Service/Enums/RoleType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.Enums
+{
+    public enum RoleType
+    {
+        Admin =1,
+        User=2,
+        Customer=3,
+        Seller=4
+    }
+}

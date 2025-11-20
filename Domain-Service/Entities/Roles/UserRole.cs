9ca06cf7a54c2b43ab2ee487ddc20084c3c004df
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-20 12:38:18 +0500

    Create UserRole.cs

---
+++ Domain-Service/Entities/Roles/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using Domain_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.Entities.Roles
+{
+    public class UserRole
+    {
+        public Guid UR_Id { get; set; }
+        public RoleType Role { get; set; }
+    }
+}

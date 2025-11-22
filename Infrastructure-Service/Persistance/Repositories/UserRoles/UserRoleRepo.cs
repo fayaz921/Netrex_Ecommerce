2f86877ab7dcc6938d62c723267df3ad971b4917
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:44:38 +0500

    Create UserRoleRepo.cs

---
+++ Infrastructure-Service/Persistance/Repositories/UserRoles/UserRoleRepo.cs
@@ -0,0 +1,18 @@
+﻿using Domain_Service.Entities.Roles;
+using Domain_Service.RepoInterfaces.UserRoles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure_Service.Persistance.Repositories.UserRoles
+{
+    public class UserRoleRepo : IUserRoleRepo
+    {
+        public Task AddUserRole(UserRole userRole)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

23f909233b02b91fe87e9b2273a67d54151464bc
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:44:23 +0500

    Create InfrastructureDIConfiguration.cs

---
+++ Infrastructure-Service/DI/Repositories_DI/InfrastructureDIConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain_Service.RepoInterfaces.UserCreadRepo;
+using Domain_Service.RepoInterfaces.UserRoles;
+using Domain_Service.RepoInterfaces.Users;
+using Infrastructure_Service.Persistance.Repositories.UserCreadRepo;
+using Infrastructure_Service.Persistance.Repositories.UserRoles;
+using Infrastructure_Service.Persistance.Repositories.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure_Service.DI.Repositories_DI
+{
+    public static class InfrastructureDIConfiguration
+    {
+        public static IServiceCollection InfrastuctureDIConfig(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepo, UserRepo>()
+                .AddScoped<IUserCreadRepo, UserCreadRepo>()
+                .AddScoped<IUserRoleRepo, UserRoleRepo>();
+
+            return services;
+        }
+    }
+}

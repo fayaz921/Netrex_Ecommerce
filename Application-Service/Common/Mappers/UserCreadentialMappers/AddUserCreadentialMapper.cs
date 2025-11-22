2decf704d8485ac334ba6156e2187d39e7dd561e
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:43:26 +0500

    Create AddUserCreadentialMapper.cs

---
+++ Application-Service/Common/Mappers/UserCreadentialMappers/AddUserCreadentialMapper.cs
@@ -0,0 +1,25 @@
+﻿using Application_Service.Services.Implementation;
+using Domain_Service.Entities.UserCreadentials;
+using Domain_Service.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Common.Mappers.UserCreadentialMappers
+{
+    public static class AddUserCreadentialMapper
+    {
+        public static UserCreadential MapForUserCread(this User user)
+        {
+            return new UserCreadential
+            {
+                Cread_Id = Guid.NewGuid(),
+                U_Id = user.U_Id,
+                OTP = new Random().Next(100000, 999999).ToString()
+
+            };
+        }
+    }
+}

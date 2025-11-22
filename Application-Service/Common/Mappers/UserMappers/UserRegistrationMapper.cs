8805d4bf746d515246f7132b9bd809b26cb42015
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:43:30 +0500

    Create UserRegistrationMapper.cs

---
+++ Application-Service/Common/Mappers/UserMappers/UserRegistrationMapper.cs
@@ -0,0 +1,24 @@
+﻿using Application_Service.DTO_s.UserDto.Account;
+using Domain_Service.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Common.Mappers.UserMappers
+{
+    public static class UserRegistrationMapper
+    {
+        public static User MapforUserReg(this UserRegisterDto dto)
+        {
+            return new User
+            {
+                U_Id = Guid.NewGuid(),
+                Email = dto.email,
+                Username = dto.username,
+                Contact = dto.contact
+            };
+        }
+    }
+}

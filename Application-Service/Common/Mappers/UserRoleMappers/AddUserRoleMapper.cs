3f57bfee49459a98a2430ac42ff412cf80ddab3a
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:43:33 +0500

    Create AddUserRoleMapper.cs

---
+++ Application-Service/Common/Mappers/UserRoleMappers/AddUserRoleMapper.cs
@@ -0,0 +1,24 @@
+﻿using Domain_Service.Entities.Roles;
+using Domain_Service.Entities.Users;
+using Domain_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Common.Mappers.UserRoleMappers
+{
+    public static class AddUserRoleMapper
+    {
+        public static UserRole MapforUserRole(this User user)
+        {
+            return new UserRole
+            {
+                UR_Id = Guid.NewGuid(),
+                U_Id = user.U_Id,
+                Role = RoleType.Customer
+            };
+        }
+    }
+}

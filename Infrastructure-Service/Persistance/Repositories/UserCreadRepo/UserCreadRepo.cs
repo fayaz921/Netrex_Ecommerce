429bf810aaee25ebb146fb7d82bd03dbd9abebe5
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:44:34 +0500

    Create UserCreadRepo.cs

---
+++ Infrastructure-Service/Persistance/Repositories/UserCreadRepo/UserCreadRepo.cs
@@ -0,0 +1,18 @@
+﻿using Domain_Service.Entities.UserCreadentials;
+using Domain_Service.RepoInterfaces.UserCreadRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure_Service.Persistance.Repositories.UserCreadRepo
+{
+    public class UserCreadRepo : IUserCreadRepo
+    {
+        public Task AddUserCread(UserCreadential usercread)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

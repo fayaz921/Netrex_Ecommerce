ec0b27c84b0a10706012b764076cd66ee65dd13d
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:44:10 +0500

    Create IUserCreadRepo.cs

---
+++ Domain-Service/RepoInterfaces/UserCreadRepo/IUserCreadRepo.cs
@@ -0,0 +1,14 @@
+﻿using Domain_Service.Entities.UserCreadentials;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.RepoInterfaces.UserCreadRepo
+{
+    public interface IUserCreadRepo
+    {
+        Task AddUserCread(UserCreadential usercread);
+    }
+}

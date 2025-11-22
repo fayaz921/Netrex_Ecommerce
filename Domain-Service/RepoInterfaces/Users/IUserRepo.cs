3e9db4e48b4f950fcba3f21b064836facb8b5b27
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:44:20 +0500

    Create IUserRepo.cs

---
+++ Domain-Service/RepoInterfaces/Users/IUserRepo.cs
@@ -0,0 +1,14 @@
+﻿using Domain_Service.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.RepoInterfaces.Users
+{
+    public interface IUserRepo
+    {
+        Task UserRegistration(User user);
+    }
+}

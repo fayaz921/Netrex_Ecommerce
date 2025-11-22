4368f9d6878b47960943c085660ee1fa70b5bdb5
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-22 11:43:41 +0500

    Create UserRegisterDto.cs

---
+++ Application-Service/DTO's/UserDto/Account/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.DTO_s.UserDto.Account
+{
+    public record UserRegisterDto(string email, string password, string username, string contact);  
+}

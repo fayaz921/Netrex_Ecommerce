7147094a16439c117fd8fdac35f1e2190f5e38e3
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-26 10:08:20 +0500

    Create IRepository.cs

---
+++ Domain-Service/RepoInterfaces/GenericRepo/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain_Service.Entities.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.RepoInterfaces.GenericRepo
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Create (T obj);
+        Task<T> Update (T obj);
+        Task<bool> Delete (T obj);
+        Task<T> GetById (Guid id);
+
+    }
+}

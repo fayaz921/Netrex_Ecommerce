4fb8a544614112c465750cba2bbe394222bfadb8
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-27 15:58:15 +0500

    Create IUnitOfWork.cs

---
+++ Domain-Service/RepoInterfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Domain_Service.Entities.Roles;
+using Domain_Service.Entities.UserCreadentials;
+using Domain_Service.Entities.Users;
+using Domain_Service.RepoInterfaces.GenericRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Service.RepoInterfaces.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IRepository<User> Users { get; }
+        IRepository<UserCreadential> Credentials { get; }
+        IRepository<UserRole> UserRoles { get; }
+        Task<int> SaveChangesAsync();
+    }
+}

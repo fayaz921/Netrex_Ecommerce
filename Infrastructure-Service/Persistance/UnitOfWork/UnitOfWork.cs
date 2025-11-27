e25c5457645bc3cb6ea5526c3d5f59877fa169a8
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-27 17:24:12 +0500

    Update UnitOfWork.cs

--- Infrastructure-Service/Persistance/UnitOfWork/UnitOfWork.cs
+++ Infrastructure-Service/Persistance/UnitOfWork/UnitOfWork.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Domain_Service.Entities.Roles;
+using Domain_Service.Entities.UserCreadentials;
+using Domain_Service.Entities.Users;
+using Domain_Service.RepoInterfaces.GenericRepo;
+using Domain_Service.RepoInterfaces.UnitOfWork;
+using Infrastructure_Service.Data;
+using Infrastructure_Service.Persistance.GenericRepo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,22 @@ using System.Threading.Tasks;
 
 namespace Infrastructure_Service.Persistance.UnitOfWork
 {
-    internal class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public IRepository<User> Users => new Repository<User>(_context);
+
+        public IRepository<UserCreadential> Credentials => new Repository<UserCreadential>(_context);
+
+        public IRepository<UserRole> UserRoles => new Repository<UserRole>(_context);
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
     }
 }

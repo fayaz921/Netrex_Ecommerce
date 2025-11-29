b28299fc7ad0202eb151372293067e14a7b4fad0
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-29 09:59:56 +0500

    Create ProductStatus.cs

---
+++ Domain-Service/Enums/ProductStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Enums
+{
+    public enum ProductStatus
+    {
+        Active =1,
+        Inactive=2,
+        OutOfStock=3,
+        Discontinued=4
+    }
+}

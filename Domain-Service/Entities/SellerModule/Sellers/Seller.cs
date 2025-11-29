b4dac997d80d58f93ad2b430db3ed9e47e7457ca
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-29 09:59:30 +0500

    Create Seller.cs

---
+++ Domain-Service/Entities/SellerModule/Sellers/Seller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.SellerModule.Sellers
+{
+    public class Seller
+    {
+        [Key]
+        public Guid SellerId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ShopId { get; set; }
+        public string StoreName { get; set; } = string.Empty;
+        public string StoreDescription { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int GstNumber { get; set; } 
+    }
+}

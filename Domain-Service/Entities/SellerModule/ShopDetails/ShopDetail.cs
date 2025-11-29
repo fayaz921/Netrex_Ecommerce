1cb99fb4005bbf376d01321a837ca788c33fe8e1
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-11-29 09:59:33 +0500

    Create ShopDetail.cs

---
+++ Domain-Service/Entities/SellerModule/ShopDetails/ShopDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.SellerModule.ShopDetails
+{
+    public class ShopDetail
+    {
+        [Key]
+        public Guid ShopId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}

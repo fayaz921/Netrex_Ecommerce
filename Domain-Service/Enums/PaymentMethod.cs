cce1380b66894eb0580058533a1c2179fd0b952a
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:19 +0500

    Create PaymentMethod.cs

---
+++ Domain-Service/Enums/PaymentMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Enums
+{
+    public enum PaymentMethod
+    {
+        easyPaisa,
+        jazzCash,
+    }
+}

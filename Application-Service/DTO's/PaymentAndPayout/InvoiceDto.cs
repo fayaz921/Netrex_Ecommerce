7eb2308cd80f066ad5fdb970efb680fd1c8f3209
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:55 +0500

    Create InvoiceDto.cs

---
+++ Application-Service/DTO's/PaymentAndPayout/InvoiceDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.DTO_s.PaymentAndPayout
+{
+    public record InvoiceDto (Guid OrderId, double Total);
+   
+}

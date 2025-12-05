2caf66d3bfc36dccd23cb099eb5fef8e51ebe01a
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:46 +0500

    Create InvoiceService.cs

---
+++ Application-Service/Services/Implementation/InvoiceService.cs
@@ -0,0 +1,26 @@
+﻿using Application_Service.Common.Mappers.PaymentAndPayout;
+using Application_Service.DTO_s.PaymentAndPayout;
+using Application_Service.Services.Interfaces;
+using Domain_Service.RepoInterfaces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Services.Implementation
+{
+    public class InvoiceService : IInvoiceService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public InvoiceService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task CreateInvoiceAsync(InvoiceDto invoiceDto)
+        {
+            var invoice = invoiceDto.Map();
+            await _unitOfWork.Invoices.Create(invoice);
+        }
+    }
+}

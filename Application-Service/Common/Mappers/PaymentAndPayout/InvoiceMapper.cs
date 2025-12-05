8f1c866a613f339840d1e8d47f71dca01e56d015
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:07 +0500

    Create InvoiceMapper.cs

---
+++ Application-Service/Common/Mappers/PaymentAndPayout/InvoiceMapper.cs
@@ -0,0 +1,24 @@
+﻿using Application_Service.DTO_s.PaymentAndPayout;
+using Application_Service.Entities.PaymentAndPayoutModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Common.Mappers.PaymentAndPayout
+{
+    public static class InvoiceMapper
+    {
+        public static Invoice Map(this InvoiceDto dto)
+        {
+           return new Invoice
+            {
+                InvoiceId = Guid.NewGuid(),
+                OrderId = dto.OrderId,
+                Total = dto.Total,
+                CreatedAt = DateTime.UtcNow
+           };
+        }
+    }
+}

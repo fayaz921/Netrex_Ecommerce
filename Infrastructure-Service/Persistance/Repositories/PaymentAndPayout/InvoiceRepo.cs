519903be7c9c533f8e73d32b51eac2f6244edb5b
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:35 +0500

    Create InvoiceRepo.cs

---
+++ Infrastructure-Service/Persistance/Repositories/PaymentAndPayout/InvoiceRepo.cs
@@ -0,0 +1,20 @@
+﻿using Application_Service.Entities.PaymentAndPayoutModule;
+using Application_Service.RepoInterfaces.PaymentAndPayout;
+using Infrastructure_Service.Data;
+
+namespace Application_Service.Persistance.Repositories.PaymentAndPayout
+{
+    public class InvoiceRepo : IInvoiceRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public InvoiceRepo(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public async Task CreateInvoice(Invoice invoice)
+        {
+            await _context.Invoices.AddAsync(invoice);
+
+        }
+    }
+}

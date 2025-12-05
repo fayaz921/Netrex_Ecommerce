13efc423e4397409abf0001c7203d94ebb11d0e1
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:14 +0500

    Create Invoice.cs

---
+++ Domain-Service/Entities/PaymentAndPayoutModule/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.PaymentAndPayoutModule
+{
+    public class Invoice
+    {
+        [Key]
+        public Guid InvoiceId { get; set; }
+        public Guid OrderId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public double Total { get; set; }
+    }
+}

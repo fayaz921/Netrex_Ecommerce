3fa2fedef72ee104391cf84541f5c9911e1975f9
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:24 +0500

    Create SellerPayout.cs

---
+++ Domain-Service/Entities/PaymentAndPayoutModule/SellerPayout.cs
@@ -0,0 +1,21 @@
+﻿using Application_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.PaymentAndPayoutModule
+{
+    public class SellerPayout
+    {
+        [Key]
+        public Guid SellerPayoutId { get; set; }
+        public Guid SellerId { get; set; }
+        public Guid OrderId { get; set; }
+        public double AmountToPay { get; set; }
+        public PaymentStatus PaymentStatus { get; set; }
+        public DateTime PayOutDate { get; set; }
+    }
+}

47baf8a0e11a450f6c747c34b9e625e17096bf9f
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:28 +0500

    Create PaymentDetail.cs

---
+++ Domain-Service/Entities/PaymentAndPayoutModule/PaymentDetail.cs
@@ -0,0 +1,23 @@
+﻿using Application_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.PaymentAndPayoutModule
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public Guid PaymentDetailId { get; set; }
+        public Guid OrderId { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public string TransactionId { get; set; } = string.Empty;
+        public PaymentStatus PaymentStatus { get; set; }
+        public double AmountPaid { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}

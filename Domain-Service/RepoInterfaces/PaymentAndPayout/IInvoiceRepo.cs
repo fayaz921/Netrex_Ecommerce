41952f0c2af8f17d4bfa9ce8f5663cc4cbc3fafe
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-05 11:59:40 +0500

    Create IInvoiceRepo.cs

---
+++ Domain-Service/RepoInterfaces/PaymentAndPayout/IInvoiceRepo.cs
@@ -0,0 +1,9 @@
+﻿using Application_Service.Entities.PaymentAndPayoutModule;
+
+namespace Application_Service.RepoInterfaces.PaymentAndPayout
+{
+    public interface IInvoiceRepo
+    {
+        public Task CreateInvoice(Invoice invoice);
+    }
+}

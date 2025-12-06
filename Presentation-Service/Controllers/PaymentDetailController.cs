12e48ced167166768a9800456a62299b12e61d69
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-06 12:23:17 +0500

    Create PaymentDetailController.cs

---
+++ Presentation-Service/Controllers/PaymentDetailController.cs
@@ -0,0 +1,26 @@
+﻿using Application_Service.DTO_s.PaymentAndPayout;
+using Application_Service.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentDetailController : ControllerBase
+    {
+        private readonly IPaymentDetailManager _paymentDetailManager;
+        public PaymentDetailController(IPaymentDetailManager paymentDetailManager)
+        {
+            _paymentDetailManager = paymentDetailManager;
+        }
+        [HttpPost("ProcessPayment")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> ProcessPayment([FromBody]AddPaymentDetailDto dto)
+        {
+            await _paymentDetailManager.AddPaymentDetailAsync(dto);
+            return Created("", "Payment processed successfully (mock integration)");
+        }
+    }
+}

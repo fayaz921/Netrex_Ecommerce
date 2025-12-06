a61aadbe0c87d98fb69296ec2c6367d4a764ee31
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-06 12:23:36 +0500

    Create AddPaymentDetailDto.cs

---
+++ Application-Service/DTO's/PaymentAndPayout/AddPaymentDetailDto.cs
@@ -0,0 +1,16 @@
+﻿using Application_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.DTO_s.PaymentAndPayout
+{
+    public class AddPaymentDetailDto
+    {
+        public Guid OrderId { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+        public double AmountPaid { get; set; }
+    }
+}

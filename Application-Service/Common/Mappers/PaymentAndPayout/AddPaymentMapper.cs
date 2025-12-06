a7dcf46e7ecaabd710ca5be57d3701bfb9120581
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-06 12:23:14 +0500

    Create AddPaymentMapper.cs

---
+++ Application-Service/Common/Mappers/PaymentAndPayout/AddPaymentMapper.cs
@@ -0,0 +1,27 @@
+﻿using Application_Service.DTO_s.PaymentAndPayout;
+using Application_Service.Entities.PaymentAndPayoutModule;
+using Application_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Common.Mappers.PaymentAndPayout
+{
+    public static class AddPaymentMapper
+    {
+        public static PaymentDetail Map(this AddPaymentDetailDto dto)
+        {
+            return new PaymentDetail
+            {
+                PaymentDetailId = Guid.NewGuid(),
+                OrderId = dto.OrderId,
+                PaymentMethod = dto.PaymentMethod,
+                AmountPaid = dto.AmountPaid,
+                PaymentStatus = PaymentStatus.Pending,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+    }
+}

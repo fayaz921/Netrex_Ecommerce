bacd47f3f59c2b5fa851db1249dba2ba99477fd6
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:50:05 +0500

    Create IUserAccountService.cs

---
+++ Application-Service/Services/UserManagmentService/Interfaces/IUserAccountService.cs
@@ -0,0 +1,14 @@
+﻿using Application_Service.DTO_s.UserDto.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Services.UserManagmentService.Interfaces
+{
+    public interface IUserAccountService
+    {
+        Task<string> UserRegisterationAsync(UserRegisterDto request);
+    }
+}

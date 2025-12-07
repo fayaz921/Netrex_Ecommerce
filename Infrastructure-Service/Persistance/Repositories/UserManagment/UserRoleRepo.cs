fd7b5a2ae007f787631c105d0f55ec9fc91c8726
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:52:20 +0500

    Create UserRoleRepo.cs

---
+++ Infrastructure-Service/Persistance/Repositories/UserManagment/UserRoleRepo.cs
@@ -0,0 +1,18 @@
+﻿using Application_Service.Entities.UserModule;
+using Application_Service.RepoInterfaces.UserManagment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Persistance.Repositories.UserManagment
+{
+    public class UserRoleRepo : IUserRoleRepo
+    {
+        public Task AddUserRole(UserRole userRole)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

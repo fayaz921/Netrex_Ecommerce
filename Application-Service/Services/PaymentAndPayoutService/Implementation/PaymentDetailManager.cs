1cfb27cf03ef5cc2b8efc2fa8bf41b92fd82bc63
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:49:24 +0500

    Create PaymentDetailManager.cs

---
+++ Application-Service/Services/PaymentAndPayoutService/Implementation/PaymentDetailManager.cs
@@ -0,0 +1,27 @@
+﻿using Application_Service.Common.Mappers.PaymentAndPayout;
+using Application_Service.DTO_s.PaymentAndPayout;
+using Application_Service.Services.PaymentAndPayoutService.Interfaces;
+using Domain_Service.RepoInterfaces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Services.PaymentAndPayoutService.Implementation
+{
+    public class PaymentDetailManager : IPaymentDetailManager
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PaymentDetailManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task AddPaymentDetailAsync(AddPaymentDetailDto dto)
+        {
+            var paymentDetail = dto.Map();
+            await _unitOfWork.PaymentDetails.Create(paymentDetail);
+            var result = await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}

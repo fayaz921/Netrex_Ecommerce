a52aa4b3688b21dde6e569d6e5de2a6b1e083899
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:51:53 +0500

    Create IUserRoleRepo.cs

---
+++ Domain-Service/RepoInterfaces/UserManagment/IUserRoleRepo.cs
@@ -0,0 +1,14 @@
+﻿using Application_Service.Entities.UserModule;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.RepoInterfaces.UserManagment
+{
+    public interface IUserRoleRepo
+    {
+        Task AddUserRole (UserRole userRole);
+    }
+}

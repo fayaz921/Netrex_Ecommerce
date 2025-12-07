fe363c13f1bbd434c9a234ca77a71f62ae5bddca
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:50:35 +0500

    Create ProductImage.cs

---
+++ Domain-Service/Entities/ProductAndCategoryModule/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.ProductAndCategoryModule
+{
+    public class ProductImage
+    {
+        [Key]
+        public Guid ProductImageId { get; set; }
+        public Guid ProductId { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public bool IsPrimary { get; set; }
+        public DateTime UploadedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}

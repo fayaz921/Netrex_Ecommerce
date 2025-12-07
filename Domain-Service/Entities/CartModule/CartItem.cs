3d0bfd49ed065c9b81e167d02ea67cf8b43c357d
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:50:17 +0500

    Create CartItem.cs

---
+++ Domain-Service/Entities/CartModule/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.CartModule
+{
+    public class CartItem
+    {
+        public Guid CartItemId { get; set; }
+        public Guid CartId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+     
+    }
+}

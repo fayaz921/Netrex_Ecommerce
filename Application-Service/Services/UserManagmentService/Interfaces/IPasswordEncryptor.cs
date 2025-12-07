d5e447a205eb47ba564ee8cf27c4f725249d5329
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:50:23 +0500

    Create IPasswordEncryptor.cs

---
+++ Application-Service/Services/UserManagmentService/Interfaces/IPasswordEncryptor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Services.UserManagmentService.Interfaces
+{
+    public interface IPasswordEncryptor
+    {
+        Task CreatePasswordHashAndSalt(string password, out byte[] passwordHash, out byte[] passwordSalt);
+        Task<bool> VerifyPassword(string password, byte[] storedHash, byte[] storedSalt);
+    }
+}

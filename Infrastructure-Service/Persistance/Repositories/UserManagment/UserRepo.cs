87a5224bb90c8a3d0225f667957a78531607c6fb
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:52:03 +0500

    Create UserRepo.cs

---
+++ Infrastructure-Service/Persistance/Repositories/UserManagment/UserRepo.cs
@@ -0,0 +1,18 @@
+﻿using Application_Service.Entities.UserModule;
+using Application_Service.RepoInterfaces.UserManagment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Persistance.Repositories.UserManagment
+{
+    public class UserRepo : IUserRepo
+    {
+        public Task UserRegistration(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

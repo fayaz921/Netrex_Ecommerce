e668ec7666b967e61b8adef4e53a5ed2c7734155
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-07 20:50:30 +0500

    Create Product.cs

---
+++ Domain-Service/Entities/ProductAndCategoryModule/Product.cs
@@ -0,0 +1,28 @@
+﻿using Application_Service.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Service.Entities.ProductAndCategoryModule.Products
+{
+    public class Product
+    {
+        [Key]
+        public Guid ProductId { get; set; }
+        public Guid SellerId { get; set; }
+        public Guid ProductCategoryId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public decimal Discount { get; set; }
+        public int StockQuantity { get; set; }
+        public ProductStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+
+    }
+}
